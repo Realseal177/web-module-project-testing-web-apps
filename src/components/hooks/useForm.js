5bdfba39fa73f3fa2f4672272cb37ea0c6f59780
Andrew Nelson <Realseal177@gmail.com>
2022-01-13 20:28:50 -0800

    totally forgot to commit but here it is finished

---
+++ src/components/hooks/useForm.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export const useForm = (initFormValue) => {
+    const [ form, setForm ] = useState(initFormValue);
+    
+    const clearForm = () => {
+        setForm(initFormValue);
+    }
+
+    const handleFormChanges = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    return [ form, handleFormChanges, clearForm ]
+}
